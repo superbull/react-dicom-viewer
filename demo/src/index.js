c57f81ba941353097bdb7c625e013c732b54ce76
Deng Zhi Cheng <superbulldeng@gmail.com>
2018-12-30 22:44:24 +0800

    show basic dicom image

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,37 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import * as cornerstone from 'cornerstone-core';
+import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
+import * as dicomParser from 'dicom-parser';
 
-import Example from '../../src'
+import MedicalImageViewer from '../../src/MedicalImageViewer';
+
+const config = {
+  webWorkerPath: '/js/cornerstoneWADOImageLoaderWebWorker.min.js',
+  taskConfiguration: {
+    decodeTask: {
+      codecsPath: '/js/cornerstoneWADOImageLoaderCodecs.min.js',
+    }
+  }
+};
+
+cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
+cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
+cornerstoneWADOImageLoader.webWorkerManager.initialize(config);
+cornerstoneWADOImageLoader.configure({
+  beforeSend: (xhr) => {
+    // add custom header here
+    // xhr.setRequestHeader('APIKEY', 'my auth token')
+  }
+});
 
 class Demo extends Component {
   render() {
     return <div>
       <h1>react-dicom-viewer Demo</h1>
-      <Example/>
+      <MedicalImageViewer 
+        url="wadouri:http://localhost:3000/dicom/series-000002/image-000010.dcm"
+      />
     </div>
   }
 }
---
+++ src/MedicalImageViewer.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react';
+import * as cornerstone from 'cornerstone-core';
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.onImageRendered = this.onImageRendered.bind(this);
+    this.onNewImage = this.onNewImage.bind(this);
+    this.onWindowResize = this.onWindowResize.bind(this);
+  }
+  componentDidMount() {
+    const element = this.element;
+
+    // Enable the DOM Element for use with Cornerstone
+    cornerstone.enable(element);
+
+    // Load the first image in the stack
+    cornerstone.loadImage(this.props.url).then(image => {
+      // Display the first image
+      cornerstone.displayImage(element, image);
+
+      // // Add the stack tool state to the enabled element
+      // const stack = this.props.stack;
+      // cornerstoneTools.addStackStateManager(element, ["stack"]);
+      // cornerstoneTools.addToolState(element, "stack", stack);
+
+      // cornerstoneTools.mouseInput.enable(element);
+      // cornerstoneTools.mouseWheelInput.enable(element);
+      // cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
+      // cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
+      // cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
+      // cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
+
+      // cornerstoneTools.touchInput.enable(element);
+      // cornerstoneTools.panTouchDrag.activate(element);
+      // cornerstoneTools.zoomTouchPinch.activate(element);
+
+      element.addEventListener(
+        "cornerstoneimagerendered",
+        this.onImageRendered
+      );
+      element.addEventListener("cornerstonenewimage", this.onNewImage);
+      window.addEventListener("resize", this.onWindowResize);
+    });
+  }
+
+  componentWillUnmount() {
+    const element = this.element;
+    element.removeEventListener(
+      "cornerstoneimagerendered",
+      this.onImageRendered
+    );
+
+    element.removeEventListener("cornerstonenewimage", this.onNewImage);
+
+    window.removeEventListener("resize", this.onWindowResize);
+
+    cornerstone.disable(element);
+  }
+
+  onWindowResize() {
+    console.log("onWindowResize");
+    cornerstone.resize(this.element);
+  }
+
+  onImageRendered() {
+    const viewport = cornerstone.getViewport(this.element);
+    console.log(viewport);
+
+    this.setState({
+      viewport
+    });
+
+    console.log(this.state.viewport);
+  }
+
+  onNewImage() {
+    const enabledElement = cornerstone.getEnabledElement(this.element);
+
+    this.setState({
+      imageId: enabledElement.image.imageId
+    });
+  }
+
+  render() {
+    return (
+      <div
+        ref={el => this.element = el}
+      >
+        
+      </div>
+    );
+  }
+}
